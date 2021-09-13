3a30c762525ee181b4ff7f650c6608abbb15120a
lydiavanderputten <84539257+lydiavanderputten@users.noreply.github.com>
2021-09-13 22:22:06 +0200

    Add files via upload

---
+++ Encoded_data_internet_children3_complete_SQL.sql
@@ -0,0 +1,15 @@
+use internet_connectivity;
+Create table internet_connectivity(
+iso3_country_code int,
+countries varchar(40),
+region varchar(40),
+subregion varchar(40),
+income_group int,
+total int,
+residence_rural varchar(40),
+residence_urban varchar(40),
+wealth_quintile_poorest varchar(40),
+wealth_quintile_richest varchar(40));
+
+
+
